762ee4d6905c905c950b4e6cc602731d0dc944bf
unknown <16663@BLRTRIBWN16663.ITCINFOTECH.com>
2016-04-15 06:55:06 +0530

    added json file

--- js/RGridAjax.js
+++ js/RGridAjax.js
@@ -1,42 +1,90 @@
-var UserGist = React.createClass({
+var FilterableProductTable = React.createClass({
+    
     getInitialState: function() {
         return {
-            name: '',
-            price: '',
-            category:''
+            products: [{company: '', price: '', gender: '', name: ''}]
         };
     },
 
-    componentDidMount: function() {
-        console.log("componentDidMount - Called");
-        console.log(this.props.source);
-        console.log("before calling serverRequest");
+    refreshData: function() {
+        // alert("click");
+        console.log("inside click");
         this.serverRequest = $.get(this.props.source, function (result) {
-            var lastGist = result[0];
-            console.log("inside serverRequest");
-            this.setState({
-                name: lastGist.name,
-                price: lastGist.price,
-                category : lastGist.category
-            });
-        }.bind(this));
-    },
-
-    componentWillUnmount: function() {
-        this.serverRequest.abort();
+            console.log("after serverRequest");
+            var lastGist = result;
+            this.setState({products:lastGist});
+        }.bind(this));        
     },
 
     render: function() {
+        console.log("inside FilterableProductTable");
         return (
           <div>
-            {this.state.name}s {this.state.price}here ID: {this.state.category}
+            <SearchBar />
+            <input type="button" value="Refresh" onClick={this.refreshData}></input>
+            <ProductTable products={this.state.products} />            
         </div>
+        );
+}
+});
+
+var ProductTable = React.createClass({
+    render: function() {
+        var rows = [];
+        var lastCategory = null;
+        this.props.products.forEach(function(product) {            
+            rows.push(<ProductRow product={product} key={product.name} category={product.category} />);
+        lastCategory = product.category;
+        });
+
+        return (
+            <div>
+              <table className="pure-table pure-table-bordered">
+                <thead>
+                  <tr>
+                    <th>Name</th>
+                    <th>Gender</th>
+                    <th>Company</th>  
+                </tr>
+                </thead>
+                <tbody>{rows}</tbody>
+              </table>            
+            </div>
+        );
+    }
+});
+
+var ProductRow = React.createClass({
+    render: function() {
+        return (
+          <tr>
+            <td>{this.props.product.name}</td>
+            <td>{this.props.product.gender}</td>
+            <td>{this.props.product.company}</td>
+          </tr>
+        );
+}
+});
+
+
+var SearchBar = React.createClass({
+    render: function() {
+        return (
+          <form>
+            <input type="text" placeholder="Search..." />
+            <p>
+              <input type="checkbox" />
+              {' '}
+        Only show products in stock
+      </p>
+    </form>
       );
     }
 });
 
+
 React.render(
-  <UserGist source="model/products.json" />,
+  <FilterableProductTable  source="https://cdn.rawgit.com/angular-ui/ui-grid.info/gh-pages/data/100.json"  />,
   document.getElementById('react-container')
 );
-
+           
